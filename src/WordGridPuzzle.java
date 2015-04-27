5f7a3524d2df23905ef6487d4f92cf642c94b8a6
Phil McGee <philip.mcgee@my.jcu.edu.au>
2015-04-27 14:01:38 +1000

    Initial commit

---
+++ src/WordGridPuzzle.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Phil on 27/04/2015.
+ */
+public class WordGridPuzzle {
+    // "//xx" = line for program development only
+    //MAIN CONTROL:
+    public static void main(String[] args){
+    System.out.print("main start : "); //xx
+        //GLOBAL main FIXED VARIABLES INITIALISATION
+        //grid size range
+        int minrows =3;
+        int mincols = minrows;
+        int maxrows =5;
+        int maxcols = maxrows;
+        System.out.print("grid size range "+minrows+"x"+mincols+" to "+maxrows+"x"+maxcols+".");
+
+
+    }//^main
+
+}//^WordGridPuzzle
